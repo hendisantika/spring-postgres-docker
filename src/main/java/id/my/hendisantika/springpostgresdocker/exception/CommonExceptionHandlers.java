b21d1d47c7cfaf9ffe10c9b16ff8402ddf278d2d
hendisantika <hendisantika@yahoo.co.id>
2025-06-07 08:34:50 +0700

    feat: Add CommonExceptionHandlers🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springpostgresdocker/exception/CommonExceptionHandlers.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.springpostgresdocker.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-postgres-docker
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 07/06/25
+ * Time: 08.34
+ * To change this template use File | Settings | File Templates.
+ */
+@RestControllerAdvice
+public class CommonExceptionHandlers {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
